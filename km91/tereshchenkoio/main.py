8613f5309477484fb3b9c368f42a986aa00d24a1
igortereshchenko <igortereshchenko@gmail.com>
2019-12-16 13:30:44 +0200

    upload

---
+++ km91/tereshchenkoio/main.py
@@ -0,0 +1,46 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="keys.json"
+
+import pandas as pd
+from bq_helper import BigQueryHelper
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+bq_assistant = BigQueryHelper(,)
+
+
+QUERY = """
+        SELECT  FROM
+        LIMIT 10000
+        """
+
+
+df = bq_assistant.query_to_pandas(QUERY)
+
+
+
+
+
+trace1 = go.Scatter(
+
+                    )
+
+
+
+trace2 = go.Pie(
+
+                    )
+
+trace3 = go.Bar(
+
+)
+
+data = [trace1]
+
+layout = dict(
+              title = '',
+              xaxis= dict(title= ''),
+              yaxis=dict(title=''),
+             )
+fig = dict(data = [trace1], layout = layout)
+plot(fig)
--- lesson3/pie.py
+++
@@ -1,9 +0,0 @@
-import plotly.graph_objs as go
-from plotly.offline import plot
-
-
-labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
-values = [4500, 2500, 1053, 500]
-
-fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
-plot(fig)
