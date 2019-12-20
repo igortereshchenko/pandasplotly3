66e76099c8490a8bd9f197c93dc54520cb2f4119
Kyparos <sasha.malakhatka@gmail.com>
2019-12-20 12:13:36 +0200

    Scatter

--- km92/MalakhatkaOV/main.py
+++ km92/MalakhatkaOV/main.py
@@ -18,17 +18,33 @@ QUERY = """
 df = bq_assistant.query_to_pandas(QUERY)
 
 trace3 = go.Bar(
-    x = df['state_number'].values,
-    y = df['condition_impairment_at_time_of_crash_driver'].values
+    x=df['state_number'].values,
+    y=df['condition_impairment_at_time_of_crash_driver'].values,
+    marker=dict(color='rgba(80, 26, 80, 0.8)')
 
+)
+trace1 = go.Scatter(
+    x=df['vehicle_number'],
+    y=df['condition_impairment_at_time_of_crash_driver']
 
 )
 
 layout = go.Layout(
-                   title=' distraction in states',
-                   xaxis=dict(title='state'),
-                   yaxis=dict( title='distraction type'),)
+    title=' distraction in states',
+    xaxis=dict(title='state'),
+    yaxis=dict(title='distraction type'), )
+ayout = dict(
+    title='Номер авто від ствну водія',
+    xaxis=dict(
+        title='Номер авто'
+    ),
+    yaxis=dict(
+        title='Стан водія',
 
-fig = dict(data = [trace3], layout = layout)
+    ))
+
+fig = dict(data=[trace3], layout=layout)
+fig1 = dict(data=[trace1],layout=ayout)
 
 plot(fig)
+plot(fig1)
