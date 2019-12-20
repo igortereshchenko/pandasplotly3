8fa912b26eee08496469cc8df5ea04c380f9275b
mashapinda <masha.pinda@gmail.com>
2019-12-20 15:57:47 +0200

    bar modified, scatter added

--- km92/pindamv/main.py
+++ km92/pindamv/main.py
@@ -11,17 +11,17 @@ bq_assistant = BigQueryHelper('bigquery-public-data', 'nhtsa_traffic_fatalities'
 QUERY = """
         SELECT *
         FROM `bigquery-public-data.nhtsa_traffic_fatalities.nmprior_2015`
-        LIMIT 100
+      
         """
 
 df = bq_assistant.query_to_pandas(QUERY)
 print(df)
 
-x = df.state_number
+x = df.vehicle_number
 
 bar = go.Bar(
     x= x,
-    y = df.non_motorist_action_circumstances,
+    y = df.sort_values(by = 'state_number', ascending=True),
     marker=dict(line=dict(color = 'green', width = 1.5)),
     name='Action circumstances'
 )
@@ -34,4 +34,19 @@ layout = go.Layout(
 fig = go.Figure(data = [bar], layout = layout)
 plot(fig)
 
+x1 = df.sort_values(by = 'state_number', ascending=True)
+scatter = go.Scatter(
+    x = x1,
+    y = df.non_motorist_action_circumstances,
+    mode = 'lines'
+)
+
+layout2 = dict(
+    title='Number of accidents in regions',
+    xaxis = dict(title = 'Regions', zeroline = True),
+    yaxis = dict(title = 'Number of accidents', zeroline = True)
+)
+
+fig2 = dict(data = [scatter], layout = layout2)
+plot(fig2)
 
