ce00e3c16220a23b4c9d8600b050f32d477e6b8e
Nikita <mykytamaliarenko@gmail.com>
2019-12-20 11:45:19 +0200

    Scatter

--- km92/maliarenkoma/main.py
+++ km92/maliarenkoma/main.py
@@ -29,5 +29,38 @@ bar_layout = dict(
     yaxis=dict(title='number'),
 )
 
-fig = dict(data=[bar], layout=bar_layout)
-plot(fig)
+plot(dict(data=[bar], layout=bar_layout))
+
+by_vehicles = [
+    df[df['vehicle_number'] == 5],
+    df[df['vehicle_number'] == 1],
+    df[df['vehicle_number'] == 9],
+]
+
+scatter_data = [el.groupby(["state_number"])["state_number"].count() for el in by_vehicles]
+
+sc_1 = go.Scatter(
+    x=scatter_data[0].index,
+    y=scatter_data[0].values,
+    name="vehicle id 5"
+)
+
+sc_2 = go.Scatter(
+    x=scatter_data[1].index,
+    y=scatter_data[1].values,
+    name="vehicle id 1"
+)
+
+sc_3 = go.Scatter(
+    x=scatter_data[2].index,
+    y=scatter_data[2].values,
+    name="vehicle id 9"
+)
+
+scatter_layout = dict(
+    title='Num Accidents by vehicle_id in state',
+    xaxis=dict(title='state', ),
+    yaxis=dict(title='number'),
+)
+
+plot(dict(data=[sc_1, sc_2, sc_3], layout=scatter_layout))
