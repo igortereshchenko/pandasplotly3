c689bbf92b046e8faa2bc45c14a2f845f5406194
eugene <sazoneugene04@gmail.com>
2019-12-20 11:44:52 +0200

    added bar plot

--- km92/SazonenkoED/main.py
+++ km92/SazonenkoED/main.py
@@ -1,7 +1,34 @@
-# import os
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="keys.json"
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="keys.json"
 
 from bq_helper import BigQueryHelper
+import plotly.graph_objs as go
+from plotly.offline import plot
 
-bq_assistant = BigQueryHelper('bigquery_public_data', 'nhtsa-traffic-fatalities')
+bq_assistant = BigQueryHelper('bigquery_public_data', 'nhtsa_traffic_fatalities')
 
+QUERY = """
+        SELECT state_number, vehicle_number, consecutive_number, driver_maneuvered_to_avoid_name FROM `bigquery-public-data.nhtsa_traffic_fatalities.maneuver_2016`
+        LIMIT 10000
+        """
+df = bq_assistant.query_to_pandas(QUERY)
+
+print(df)
+
+
+reason_groupby = df.groupby(['driver_maneuvered_to_avoid_name'])['state_number'].count()
+
+fig2 = {
+  "data": [
+    {
+      "y": reason_groupby.values,
+      "x": reason_groupby.index,
+      "name": "reasons of fatalities",
+      "type": "bar"
+    },
+  ],
+  "layout": {
+        "title":"reasons of fatalities",
+    }
+}
+plot(fig2)
