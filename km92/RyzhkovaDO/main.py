d4d50671e27175d311c758a336a77245558a5718
dasha-ryzhkova <ryzhkovadasha2002@.gmail.com>
2019-12-21 14:09:45 +0200

    Fixed Scatter

--- km92/RyzhkovaDO/main.py
+++ km92/RyzhkovaDO/main.py
@@ -7,27 +7,17 @@ import plotly.graph_objs as go
 from plotly.offline import plot
 bg_assistant = BigQueryHelper('bigquery-public-data', 'nhtsa_traffic_fatalities')
 QUERY = """
-         SELECT * FROM `bigquery-public-data.nhtsa_traffic_fatalities.maneuver2016`
-         LIMIT 10
+         SELECT * FROM  `bigquery-public-data.nhtsa_traffic_fatalities.maneuver_2016`
+        LIMIT 1000
         """
 df = bg_assistant.query_to_pandas(QUERY)
 trace1 = go.Scatter(
-                    x = df['state_number - назва штату'],
-                    y = df['vehicle_number'],
-                    mode = "lines",
+                    x=df['state_number'],
+                    y=df['vehicle_number'],
+                    mode="lines",
                     )
 
-fig=dict(data=[trace1])
+fig = dict(data=[trace1])
 plot(fig)
 
-trace2= go.Bar(
-    x=df["vehicle_number"],
-    y=df["driver_maneuvered_to_avoid_name"],
-            )
-fig=go.Figure(data=[trace2])
-plot(fig)
-
-trace3=go.Figure(data=[go.Pie(labels= df["driver_maneuvered_to_avoid_name"], values= df["vehicle_number"])])
-
-plot(trace3)
 
