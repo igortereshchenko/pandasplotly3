eaca4081a2c44a92ee5ad98537e8435986a5841b
sashakorets <sashakoretsua@gmail.com>
2019-12-22 00:20:13 +0200

    update file main.py all graphs work

--- km92/koretsom/main.py
+++ km92/koretsom/main.py
@@ -18,37 +18,41 @@ QUERY = """
 
 df = bq_assistant.query_to_pandas(QUERY)
 
-print(df.head(10))
-
-grink_man = df[df['number_of_drunk_drivers']=='1']
-angrink_man = df[df['number_of_drunk_drivers']=='0']
-#number_of_drunk_drivers = grink_man.groupby(angrink_man)
-
-x = len(grink_man)
-y = len(angrink_man)
-labels = ['grink_man','angrink_man']
-values = [15,4]
-# fig = go.Figure(data=[go.Pie(labels = labels,values = values)])
-# plot(fig)
-
-bar = go.Bar(x = labels, y = values)
-fig = go.Figure(data = [bar])
+print(df.head(100))
 
 
+trace1 = go.Scatter(
+    x = list(i for i in range(1,32)),
+    y = [df.day_of_crash[df.day_of_crash==i].count() for i in range(1,32)],
+    mode = "lines",
+)
+layout1 = go.Layout(
+    title = "crash on a particular day of the month",
+    xaxis = dict(title = "day on mounth"),
+    yaxis = dict(title = "number of crash")
+)
 
-# trace1 = go.Scatter()
-#
-# trace2 = go.Pie()
-#
-# trace3 = go.Bar()
-#
-#
-# data = [trace1]
-#
-# layout = dict(
-#               title = '',
-#               xaxis= dict(title= ''),
-#               yaxis=dict(title=''),
-#              )
-# fig = dict(data = [trace1], layout = layout)
-# plot(fig)
+trace2 = go.Pie(
+    # кругова діаграма, ця діаграма показує скільки відсотків дтп відбуваються з участі людини в стані алкогольного спяніння
+    labels = ['grink_man','angrink_man'],
+    values = [df.number_of_drunk_drivers[df.number_of_drunk_drivers==1].count(),df.number_of_drunk_drivers[df.number_of_drunk_drivers==0].count()]
+)
+layout2 = go.Layout(
+    title = "crash with drink drivers",
+)
+
+trace3 = go.Bar(
+    x = ['sunday','monday','tuesday','wensday','tharsday','friday','saturday'],
+    y = [df.day_of_week[df.day_of_week==1].count(),df.day_of_week[df.day_of_week==2].count(),df.day_of_week[df.day_of_week==3].count(),df.day_of_week[df.day_of_week==4].count(),df.day_of_week[df.day_of_week==5].count(),df.day_of_week[df.day_of_week==6].count(),df.day_of_week[df.day_of_week==7].count()]
+)
+layout3 = go.Layout(
+    # стопчикова діаграма, показує в який день тижня найбільше дтп
+    title = "day of week",
+    xaxis = dict(title = "day 0f week"),
+    yaxis = dict(title = "number of crash")
+)
+
+data = [trace1]
+layout = layout1
+fig = dict(data = data, layout = layout)
+plot(fig)
